ae707685071d6dcae17b4a1d350b82437b2683b0
Petros Laskas <laskasp15@gmail.com>
2024-01-19 17:31:44 +0200

    init stores

--- vue/tennisacademy/src/stores/courts.js
+++ vue/tennisacademy/src/stores/courts.js
@@ -1,2 +1,9 @@
 import { defineStore } from "pinia";
 
+export const useCourtsStore = defineStore('courts', {
+  state: () => ([
+    {id: 0, name: 'TENNIS01', available: []},
+    {id:1, name: 'TENNIS02', available: []},
+    {id:2, name: 'PADEL01', available: []}
+  ])
+})
