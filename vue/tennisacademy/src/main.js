7b741a55d29f0795d73d713bcba9ef8323fe8f88
Petros Laskas <laskasp15@gmail.com>
2024-01-22 16:03:43 +0200

    Init Base Card and Base Button

--- vue/tennisacademy/src/main.js
+++ vue/tennisacademy/src/main.js
@@ -6,9 +6,15 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 
+import BaseCard from "@/components/base/BaseCard.vue";
+import BaseButton from "@/components/base/BaseButton.vue";
+
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+
+app.component("base-card", BaseCard);
+app.component("base-button", BaseButton);
 
 app.mount("#app");
--- vue/tennisacademy/src/stores/courts.js
+++ vue/tennisacademy/src/stores/courts.js
@@ -2,8 +2,8 @@ import { defineStore } from "pinia";
 
 export const useCourtsStore = defineStore('courts', {
   state: () => ([
-    {id: 0, name: 'TENNIS01', available: []},
-    {id:1, name: 'TENNIS02', available: []},
-    {id:2, name: 'PADEL01', available: []}
+    { id: 0, name: "TENNIS01", available: [new Date()] },
+    { id: 1, name: "TENNIS02", available: [new Date()] },
+    { id: 2, name: "PADEL01", available: [new Date()] }
   ])
 })
